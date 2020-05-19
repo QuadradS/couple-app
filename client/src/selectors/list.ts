1c5731677db34d4bcc18b18609619fcd71dfc1c9
Danil <quadrad4@gmail.com>
2020-05-19 12:03:37 +0600

    removed any type

--- client/src/selectors/list.ts
+++ client/src/selectors/list.ts
@@ -1,7 +1,8 @@
 import {createSelector} from 'reselect'
 import {IList} from "../reducers/list";
+import {IStore} from "../reducers";
 
-const getListStore = (store: any) => store.list
+const getListStore = (store: IStore) => store.list
 
 export const getListData = createSelector(
     getListStore,
